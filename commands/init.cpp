0ce8992abfc2b6473ea8276818949a2d4519af5d
Akshaya-Sri-Tech <270252488+Akshaya-Sri-Tech@users.noreply.github.com>
2026-06-18 11:41:00 +0530

    init command added

--- commands/init.cpp
+++ commands/init.cpp
@@ -1,8 +1,28 @@
 #include<iostream>
+#include<filesystem>
+#include<fstream>
+#include "init.h"
 using namespace std;
 
+namespace fs = std::filesystem;
+
 void init()
- {
+{
+    if(fs::exists(".gitlite"))
+    {
+        cout << "Repository already exists." << endl;
+        return;
+    }
+    else
+    {
+        fs::create_directory(".gitlite");
+        fs::create_directory(".gitlite/staging");
+        fs::create_directory(".gitlite/commits");
+        ofstream metadataFile(".gitlite/metadata.txt");
+        metadataFile << "GitLite repository initialized" << endl;
+        metadataFile.close();
+        cout << "Initialized empty GitLite repository in .gitlite" << endl;
+    }
     
- }
+}
  
