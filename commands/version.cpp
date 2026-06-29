52b5dae2111ba158ffa3623928bec07a5276ba51
Akshaya-Sri-Tech <270252488+Akshaya-Sri-Tech@users.noreply.github.com>
2026-06-29 21:19:34 +0530

    updated version

--- commands/version.cpp
+++ commands/version.cpp
@@ -3,6 +3,6 @@ using namespace std;
  
 void version()
  {
-     cout << "gitlite version 1.0.0" << endl;
+     cout << "gitlite version 1.0.2" << endl;
      cout << "Commands available: init, add, status, commit, log, checkout, version" << endl;
  }
